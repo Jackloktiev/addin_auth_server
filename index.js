4b40fb703caf2274a190f6c56f7e5cbb26d93cf4
Jack Loktiev <jack.loktiev@gmail.comm>
2021-11-29 19:03:59 -0500

    Getting user email from MS graph having token and user ID

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const Express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
+const axios = require("axios");
 
 const App = Express();
-App.use(bodyParser.urlencoded({extended:true}));
+App.use(bodyParser.urlencoded({ extended: true }));
 App.use(Express.json());
 App.use(cors());
 
-App.get("/hello",function(req,res){
-    res.send('Hello world!');
-    
+App.get("/hello", function (req, res) {
+  res.send("Hello world!");
 });
 
-App.get("/gettemplate",function(req,res){
-    console.log(req.headers.authorization);
-    console.log(req.query.file);
-    res.send('Hello world!');
+App.get("/gettemplate", function (req, res) {
+  console.log(req.headers.authorization);
+  console.log(req.query.file);
+  console.log(req.query.userID);
+  // Get user email from MS graph
+  //   send get request to https://graph.microsoft.com/v1.0/users/{req.query.userID}
+  // with authorization header = 'Bearer {token}'
+  axios.get(`https://graph.microsoft.com/v1.0/users/{${req.query.userID}}`,{
+    headers: {
+      'Authorization': `Bearer ${req.headers.authorization}`
+        }
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  res.send("Hello world!");
 });
 
 const port = process.env.PORT || 8000;
 App.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
